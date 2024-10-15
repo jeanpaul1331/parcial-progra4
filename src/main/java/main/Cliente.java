a6a71266c125cb5911b02b4c23146136483081bd
mars77777777777777777 <150204725+mars77777777777777777@users.noreply.github.com>
2024-10-14 22:50:19 -0500

    CLIENTE FINAL LISTO

--- src/main/java/main/Cliente.java
+++ src/main/java/main/Cliente.java
@@ -27,35 +27,6 @@ public class Cliente {
         return reservas;
     }
 
-    package main;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Cliente {
-    private String idCliente;
-    private String nombre;
-    private List<Reserva> reservas;
-
-    public Cliente(String idCliente, String nombre) {
-        this.idCliente = idCliente;
-        this.nombre = nombre;
-        this.reservas = new ArrayList<>();
-    }
-
-    public String getIdCliente() {
-        return idCliente;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public List<Reserva> getReservas() {
-        return reservas;
-    }
-
     public boolean tieneReservaActiva() {
         
         for (Reserva reserva : reservas) {
